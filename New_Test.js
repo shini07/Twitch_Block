b16d30f6d3e2f3f761886da2f6129987eedfc84e
shini07 <chuck.shini1337@gmail.com>
2020-11-08 16:06:14 -0500

    Update New_Test.js

--- New_Test.js
+++ New_Test.js
@@ -5,7 +5,17 @@ window.fetch = (url, init, ...args) => {
     if (url.includes("/access_token")) {
       url = url.replace("player_type=site", "player_type=mobile");
       url = url.replace("platform=web", "platform=android");
-    }
+    }  else if (
+			url.includes("/gql") &&
+			init &&
+			typeof init.body === "string" &&
+			init.body.includes("PlaybackAccessToken")
+		) {
+			 const newBody = JSON.parse(init.body);
+		 	 newBody.variables.playerType = "mobile";
+       newBody.variables.platform = "android";
+		   init.body = JSON.stringify(newBody);
+		}
   }
   return origFetch(url, init, ...args);
 };
